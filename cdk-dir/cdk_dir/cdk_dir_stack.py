013c4b840502232b3e17e7f780b97d2bf763d1c2
bennypickard <BennyPickard@Gmail.com>
2024-12-04 14:12:43 -0800

    Add vpc, ec2, sqs

--- cdk-dir/cdk_dir/cdk_dir_stack.py
+++ cdk-dir/cdk_dir/cdk_dir_stack.py
@@ -1,9 +1,11 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
+
 
 class CdkDirStack(Stack):
 
@@ -12,8 +14,18 @@ class CdkDirStack(Stack):
 
         # The code that defines your stack goes here
 
+        # Create a VPC
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=3)
+
+        # Create an EC2 instance
+        ec2.Instance(self, 
+                     "MyInstance",
+                     instance_type=ec2.InstanceType("t2.micro"),
+                     machine_image=ec2.AmazonLinuxImage(),
+                     vpc=vpc)
+
         # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkDirQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        queue = sqs.Queue(
+            self, "CdkDirQueue",
+            visibility_timeout=sqs.Duration.seconds(300),
+        )
